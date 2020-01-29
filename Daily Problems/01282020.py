aa635ae2ef73aead53f8614ea378da50c10a4993
Mitch Sturba <57732799+MitchellSturba@users.noreply.github.com>
2020-01-28 23:51:24 -0500

    Computer Store Problem
    Finds the most expensive pairing of keyboard and drive that's still affordable to the customer

---
+++ Daily Problems/01282020.py
@@ -0,0 +1,44 @@
+#!/bin/python3
+#A problem I solved on hackerRank for the most expensive pairing of keyboards and drives afforadable to the person
+import os
+import sys
+
+#
+# Complete the getMoneySpent function below.
+#
+def getMoneySpent(keyboards, drives, b):
+    amountsToSpend = []
+    for k in keyboards:
+        if k <= b:
+            for d in range(len(drives)):
+                if k + drives[d] <= b:
+                    amountsToSpend.append(k + drives[d])
+    if len(amountsToSpend) > 0:
+        return max(amountsToSpend)
+    else:
+        return -1
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    bnm = input().split()
+
+    b = int(bnm[0])
+
+    n = int(bnm[1])
+
+    m = int(bnm[2])
+
+    keyboards = list(map(int, input().rstrip().split()))
+
+    drives = list(map(int, input().rstrip().split()))
+
+    #
+    # The maximum amount of money she can spend on a keyboard and USB drive, or -1 if she can't purchase both items
+    #
+
+    moneySpent = getMoneySpent(keyboards, drives, b)
+
+    fptr.write(str(moneySpent) + '\n')
+
+    fptr.close()
